dac0f58b0cdc215f1b857d02731044d6429f451f
anewtonllt <53426472+anewtonllt@users.noreply.github.com>
2022-09-18 21:19:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,13 +30,13 @@ cameraTrigger.onclick = function() {
     if (sw2) {
       document.getElementById("nfy").innerHTML = "Sending,.";
       document.getElementById('camera--trigger').innerHTML = 'Stop';
-      document.getElementById('camera--trigger').style.backgroundColor = 'white';
-      document.getElementById('camera--trigger').style.Color = 'black';
+      document.getElementById('camera--trigger').style.background-color = 'white';
+      document.getElementById('camera--trigger').style.color = 'black';
     } else {
       document.getElementById("nfy").innerHTML = "Ready";
       document.getElementById('camera--trigger').innerHTML = 'Send';
-      document.getElementById('camera--trigger').style.backgroundColor = 'black';
-      document.getElementById('camera--trigger').style.Color = 'white';
+      document.getElementById('camera--trigger').style.background-color = 'black';
+      document.getElementById('camera--trigger').style.color = 'white';
     }  
 //    cameraSensor.width = cameraView.videoWidth;
 //    cameraSensor.height = cameraView.videoHeight;
